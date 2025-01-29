918e9a4bb884bb73c25d8d98183b0d1e1c9ffc71
Mohammad Shadab <120562941+Imshad18@users.noreply.github.com>
2025-01-29 12:27:12 +0530

    Update command.sql

--- command.sql
+++ command.sql
@@ -1,11 +1,52 @@
 SQL is case insensitive, still I will use all caps for SQL keywords
 
-There is a table called Customers which has many columsns like CustomerID, CustomerName, City, etc
-
+-- We have tables called Customers & Products which has many columns like CustomerID, CustomerName, City, etc
 
 SELECT * FROM Customers;   -- this is SELECT Everything
-This will give the entire table 
+-- This will give the entire table 
 
 SELECT CustomerName, City FROM Customers;
-will returen CustomerName & City columns
+-- will returen CustomerName & City columns
+
+DISTINCT
+SELECT DISTINCT City FROM Customers;
+-- will only choose distinct entries and skip the repeated ones
+
+SELECT COUNT(DISTINCT City) FROM Customers;
+-- this gives us the total number of enquired objects
+
+--for MS Access 
+SELECT COUNT(*) AS DistinctCountry FROM (SELECT DISTINCT Country FROM Customers);
+
+
+WHERE
+--The WHERE clause is used to filter records. It is used to extract only those records that fulfill a specified condition
+SELECT * FROM Customers WHERE Country='Mexico';
+SELECT * FROM Customers WHERE CustomerID > 80;   -- >< operators can be used
+
+ORDER BY
+SELECT column1, column2, ...
+FROM table_name
+ORDER BY column1, column2, ... ASC|DESC;
+
+SELECT * FROM Products ORDER BY Price;
+
+SELECT * FROM Products ORDER BY productname DESC; --for alphabets
+
+SELECT * FROM Customers ORDER BY Country, CustomerName;   -- here if country name is same then it orders by the name of CustomerName 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
