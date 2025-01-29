20247d34433476100ed8b3bc747d0305e882a314
Mohammad Shadab <120562941+Imshad18@users.noreply.github.com>
2025-01-29 11:13:40 +0530

    Create command.sql

---
+++ command.sql
@@ -0,0 +1,11 @@
+SQL is case insensitive, still I will use all caps for SQL keywords
+
+There is a table called Customers which has many columsns like CustomerID, CustomerName, City, etc
+
+
+SELECT * FROM Customers;   -- this is SELECT Everything
+This will give the entire table 
+
+SELECT CustomerName, City FROM Customers;
+will returen CustomerName & City columns
+
